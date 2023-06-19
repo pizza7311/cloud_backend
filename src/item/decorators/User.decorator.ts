aaa5ea58e6f9c65f4bb1c5b86311177dfaa2bc06
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2023-06-19 23:04:23 +0900

    commnet: Add doc comment

--- src/item/decorators/User.decorator.ts
+++ src/item/decorators/User.decorator.ts
@@ -2,6 +2,10 @@ import { createParamDecorator } from '@nestjs/common/decorators';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { ExecutionContext } from '@nestjs/common';
 
+/**
+ * req.user 정보 가져오기
+ * @param {('uid'|'access_level')} key 'uid' 와 'access_level' 만 가능
+ */
 export const User = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const user = GqlExecutionContext.create(ctx).getContext().req.user;
